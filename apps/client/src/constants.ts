06803e4a9a7533c262541b419bad5442f5ba91a7
Prince gupta <26LBCO3004@culkomail.in>
2026-09-10 23:52:45 +0530

    Update GitHub repository link to princeff55032-maker/Musicsync

--- apps/client/src/constants.ts
+++ apps/client/src/constants.ts
@@ -1,4 +1,4 @@
 export const SOCIAL_LINKS = {
   discord: "https://discord.beatsync.gg",
-  github: "https://github.com/freeman-jiang/beatsync",
+  github: "https://github.com/princeff55032-maker/Musicsync",
 } as const;
